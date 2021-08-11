6322f4f95aeab825488b9262135920699c20119e
Michael Lamula <micheallamula@gmail.com>
2021-08-11 15:11:03 +0200

    Initialize repo

---
+++ AppointmentScheduler-574f637f59426891cf77bdd602b3444da9e6ef15/AppointmentScheduling/Utility/EmailSender.cs
@@ -0,0 +1,38 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            MailjetClient client = new MailjetClient("064d6a04d1aa539a4178fb2da6876ccc", "cf4ba55f7d60b59b201d09d18e68b214")
+            {
+
+            };
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+           .Property(Send.FromEmail, "ethekwinimedicalpractice@gmail.com")
+           .Property(Send.FromName, "EthekwiniMedicalPractice")
+           .Property(Send.Subject, subject)
+           .Property(Send.HtmlPart, htmlMessage)
+           .Property(Send.Recipients, new JArray {
+                new JObject {
+                 {"Email", email}
+                 }
+               });
+            MailjetResponse response = await client.PostAsync(request);
+
+
+        }
+    }
+}
---
+++ AppointmentScheduler-574f637f59426891cf77bdd602b3444da9e6ef15/AppointmentScheduling/Utility/Helper.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Utility
+{
+    public static class Helper
+    {
+        public static string Admin = "Admin";
+        public static string Patient = "Patient";
+        public static string Doctor = "Doctor";
+        public static string appointmentAdded = "Appointment added successfully.";
+        public static string appointmentUpdated = "Appointment updated successfully.";
+        public static string appointmentDeleted = "Appointment deleted successfully.";
+        public static string appointmentExists = "Appointment for selected date and time already exists.";
+        public static string appointmentNotExists = "Appointment not exists.";
+        public static string meetingConfirm = "Appointment confirmed successfully.";
+        public static string meetingConfirmError = "Error while confirming meeting.";
+        public static string appointmentAddError = "Something went wrong, please try again.";
+        public static string appointmentUpdatError = "Something went wrong, please try again.";
+        public static string somethingWentWrong = "Something went wrong, please try again.";
+        public static int success_code = 1;
+        public static int failure_code = 0;
+
+        public static List<SelectListItem> GetRolesForDropDown(bool isAdmin)
+        {
+            if (isAdmin)
+            {
+                return new List<SelectListItem>
+                {
+                    new SelectListItem{Value=Helper.Admin,Text=Helper.Admin},
+                    new SelectListItem{Value=Helper.Doctor,Text=Helper.Doctor},
+                    new SelectListItem{Value=Helper.Patient,Text=Helper.Patient}
+                };
+            }
+            else
+            {
+                return new List<SelectListItem>
+                {
+                    new SelectListItem{Value=Helper.Patient,Text=Helper.Patient}
+                };
+            }
+        }
+
+        public static List<SelectListItem> GetTimeDropDown()
+        {
+            int minute = 30;
+            List<SelectListItem> duration = new List<SelectListItem>();
+            duration.Add(new SelectListItem { Value = minute.ToString(), Text = "30 minutes" });
+            minute = minute + 15;
+            duration.Add(new SelectListItem { Value = minute.ToString(), Text = "45 minutes" });
+            minute = minute + 15;
+            duration.Add(new SelectListItem { Value = minute.ToString(), Text = "1 Hour" });
+            minute = minute + 15;
+            duration.Add(new SelectListItem { Value = minute.ToString(), Text = "1 Hour 15 minutes" });
+            minute = minute + 15;
+            duration.Add(new SelectListItem { Value = minute.ToString(), Text = "1 Hour 30 minutes" });
+            minute = minute + 15;
+            duration.Add(new SelectListItem { Value = minute.ToString(), Text = "1 Hour 45 minutes" });
+            minute = minute + 15;
+            duration.Add(new SelectListItem { Value = minute.ToString(), Text = "2 Hours " });
+          
+
+
+            return duration;
+        }
+    }
+}
